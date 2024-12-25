9406011953f67dd3b01daa136d4aa2a2ea6730e4
bhuynhdev <bhuynhdev@gmail.com>
2024-12-25 18:43:25 -0500

    Mass prettier formatting

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,6 @@
-import { drizzle } from "drizzle-orm/libsql";
-import { createClient } from "@libsql/client";
+import { drizzle } from 'drizzle-orm/libsql'
+import { createClient } from '@libsql/client'
 
-if (!process.env.DB_URl) throw new Error("DB_URL is not set");
-const client = createClient({ url: process.env.DB_URl });
-export const db = drizzle(client);
+if (!process.env.DB_URl) throw new Error('DB_URL is not set')
+const client = createClient({ url: process.env.DB_URl })
+export const db = drizzle(client)
--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,63 +1,60 @@
-import { drizzle } from "drizzle-orm/libsql";
-import { createClient } from "@libsql/client";
-import { event, participant } from "./schema";
-import "dotenv/config";
+import { drizzle } from 'drizzle-orm/libsql'
+import { createClient } from '@libsql/client'
+import { event, participant } from './schema'
+import 'dotenv/config'
 
 async function main() {
-  const dbUrl = process.env.DB_URL;
+  const dbUrl = process.env.DB_URL
   if (!dbUrl) {
-    throw new Error("Missing environment variable: DB_URL");
+    throw new Error('Missing environment variable: DB_URL')
   }
-  const queryClient = createClient({ url: dbUrl });
-  const db = drizzle(queryClient);
+  const queryClient = createClient({ url: dbUrl })
+  const db = drizzle(queryClient)
 
   /**
    * SEED REGISTRY
    **/
 
-  type NewParticipant = typeof participant.$inferInsert;
+  type NewParticipant = typeof participant.$inferInsert
   const participantSeeds: Array<NewParticipant> = [
     {
-      firstName: "Steve",
-      lastName: "Job",
-      email: "steve.job@email.com",
-      checkedIn: true,
+      firstName: 'Steve',
+      lastName: 'Job',
+      email: 'steve.job@email.com',
+      checkedIn: true
     },
-    { firstName: "Andrej", lastName: "Karpathy", email: "ak@email.com" },
+    { firstName: 'Andrej', lastName: 'Karpathy', email: 'ak@email.com' },
     {
-      firstName: "Sundar",
-      lastName: "Pichai",
-      email: "pichai123@email.com",
+      firstName: 'Sundar',
+      lastName: 'Pichai',
+      email: 'pichai123@email.com'
     },
     {
-      firstName: "Hans",
-      lastName: "Zimmer",
-      email: "zimmerhans@email.com",
+      firstName: 'Hans',
+      lastName: 'Zimmer',
+      email: 'zimmerhans@email.com'
     },
-    { firstName: "Dan", lastName: "Abramov", email: "dan-react@email.com" },
-  ];
+    { firstName: 'Dan', lastName: 'Abramov', email: 'dan-react@email.com' }
+  ]
 
-  console.log("Registry Seed start");
+  console.log('Registry Seed start')
   for (const participantSeed of participantSeeds) {
     await db.transaction(async (tx) => {
-      const [newParticipant] = await tx
-        .insert(participant)
-        .values(participantSeed)
-        .returning();
-      if (!newParticipant) throw Error("Cannot create registry record");
+      const [newParticipant] = await tx.insert(participant).values(participantSeed).returning()
+      if (!newParticipant) throw Error('Cannot create registry record')
       // Insert corresponding event
       await tx.insert(event).values([
         {
           targetParticipantId: newParticipant.id,
           timestamp: new Date().toISOString(),
-          description: "Seed register participant",
-        },
-      ]);
-    });
+          description: 'Seed register participant'
+        }
+      ])
+    })
   }
 
-  console.log("Registry Seed done");
-  queryClient.close();
+  console.log('Registry Seed done')
+  queryClient.close()
 }
 
-main();
+main()
