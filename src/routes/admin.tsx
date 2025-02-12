5099d34eee382c56b011a0df1a39bed287b1fe56
bhuynhdev <bhuynhdev@gmail.com>
2025-02-11 20:22:42 -0500

    Retrieve and construct navbar links from 'email_templates' folder

--- src/routes/admin.tsx
+++ src/routes/admin.tsx
@@ -1,13 +1,27 @@
-import { RouteSectionProps, useLocation } from '@solidjs/router'
+import { A, createAsync, query, RouteSectionProps } from '@solidjs/router'
 import { TbChalkboard } from 'solid-icons/tb'
+import { convertPathToModuleName } from '~/utils'
 
-const links = [
-	{ title: 'Participants', href: '/admin/participants' },
-	{ title: 'Emails', href: '/admin/emails' }
-]
+const getLinks = query(async () => {
+	'use server'
+	return [
+		{ title: 'Participants', href: '/admin/participants' },
+		{
+			title: 'Emails',
+			sublinks: Object.keys(import.meta.glob('../email_templates/*.tsx')).map((path) => {
+				const moduleName = convertPathToModuleName(path)
+				return { title: moduleName, href: `/admin/emails/${moduleName}` }
+			})
+		}
+	]
+}, 'nav-links')
+
+export const route = {
+	preload: () => getLinks()
+}
 
 export default function AdminLayout(props: RouteSectionProps) {
-	const location = useLocation()
+	const links = createAsync(() => getLinks())
 	return (
 		<div class="grid min-h-screen grid-rows-[auto_1fr_auto]">
 			<header class="bg-diagonal-pattern border-b p-4">
@@ -17,11 +31,26 @@ export default function AdminLayout(props: RouteSectionProps) {
 				{/* Left sidebar */}
 				<nav class="w-64 py-4" aria-label="Main navigation">
 					<ul class="menu w-full space-y-1">
-						{links.map((link) => (
+						{links()?.map((link) => (
 							<li>
-								<a href={link.href} class="px-4 py-2" classList={{ 'menu-active': location.pathname === link.href }}>
-									<span>{link.title}</span>
-								</a>
+								{link.href ? (
+									<a href={link.href} class="px-4 py-2">
+										<span>{link.title}</span>
+									</a>
+								) : (
+									<p class="px-4 py-2">{link.title}</p>
+								)}
+								{link.sublinks && (
+									<ul>
+										{link.sublinks.map((l) => (
+											<li>
+												<a href={l.href} class="px-4 py-2">
+													<span>{l.title}</span>
+												</a>
+											</li>
+										))}
+									</ul>
+								)}
 							</li>
 						))}
 					</ul>
