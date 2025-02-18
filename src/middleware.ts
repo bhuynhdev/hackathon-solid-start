e7188cf870186def1bd297c7e0ab0c44791b981f
bhuynhdev <bhuynhdev@gmail.com>
2025-02-17 22:41:13 -0500

    Leverage getPlatformProxy to run D1 in both dev and prod

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,14 @@
 import { createMiddleware } from '@solidjs/start/middleware'
 
 import { drizzle as drizzleD1 } from 'drizzle-orm/d1'
-import { drizzle as drizzleLibsql } from 'drizzle-orm/libsql'
-import * as schema from '~/db/schema'
 
 export default createMiddleware({
 	onRequest: async (event) => {
 		if (!event.locals.db) {
 			if (import.meta.env.DEV) {
-				const db = drizzleLibsql(process.env.DB_URL!, { schema: schema })
+				const { getPlatformProxy } = await import('wrangler')
+				const proxy = await getPlatformProxy()
+				const db = drizzleD1(proxy.env.DB as D1Database)
 				event.locals.db = db
 			} else {
 				const db = drizzleD1(event.nativeEvent.context.cloudflare.env.DB)
