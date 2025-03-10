482b374ff93e3cc0ab4bc6e6c93317d2b726661c
bhuynhdev <bhuynhdev@gmail.com>
2025-03-09 23:20:48 -0400

    Add reset button to AddCategories form

--- src/features/judging/AddCategoriesForm.tsx
+++ src/features/judging/AddCategoriesForm.tsx
@@ -6,6 +6,7 @@ interface AddCategoriesFormProps {
 }
 
 export function AddCategoriesForm(props: AddCategoriesFormProps) {
+	let formRef!: HTMLFormElement
 	return (
 		<>
 			<h3 class="mb-4 text-lg font-bold">Add Categories</h3>
@@ -15,7 +16,7 @@ export function AddCategoriesForm(props: AddCategoriesFormProps) {
 
 				<input type="radio" name="my_tabs_2" class="tab" aria-label="Bulk entry" checked />
 				<div class="tab-content border-base-300 bg-base-100 p-5">
-					<form method="post" class="space-y-3" action={bulkCreateCategories} enctype="multipart/form-data">
+					<form method="post" class="space-y-3" action={bulkCreateCategories} enctype="multipart/form-data" ref={formRef}>
 						<p>Upload categories data as a CSV file</p>
 						<input type="file" name="csvFile" class="file-input" aria-label="Upload CSV file" />
 						<p>Or enter categories as comma-separated strings</p>
@@ -25,15 +26,23 @@ export function AddCategoriesForm(props: AddCategoriesFormProps) {
 							class="textarea w-full"
 							placeholder="category1,sponsor&#10;category2,inhouse"
 						/>
-						<button type="submit" class="btn btn-primary ml-auto block">
-							Submit
-						</button>
+						<div class="space-x-3 text-right">
+							<button type="submit" class="btn btn-neutral btn-outline" onclick={() => formRef.reset()}>
+								Reset
+							</button>
+							<button type="submit" class="btn btn-primary">
+								Submit
+							</button>
+						</div>
 						<div class="rounded-md border border-blue-400 bg-blue-100 p-3 text-sm">
 							<p class="font-semibold">
 								<IconTablerInfoCircle class="inline align-text-bottom" /> CSV format
 							</p>
-							<p>First column is category name</p>
-							<p>Second column is type - 'sponsor' or 'inhouse'</p>
+							<p>First column: category name</p>
+							<p>Second column: type - 'sponsor' or 'inhouse'</p>
+							<p>
+								<span class="font-semibold">Note:</span> Don't provide headers. Entries will override existing categories if same name
+							</p>
 						</div>
 					</form>
 				</div>
