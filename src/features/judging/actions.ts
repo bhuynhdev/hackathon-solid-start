289aee05c94eb1702a141e4e48fc339b792d7f19
bhuynhdev <baoh.inbox@gmail.com>
2025-07-08 00:50:10 -0400

    Fully implement Assign Projects to Groups action

--- src/features/judging/actions.ts
+++ src/features/judging/actions.ts
@@ -5,6 +5,7 @@ import { eq, getTableColumns, inArray, notInArray, sql } from 'drizzle-orm'
 import { assignment, category, categoryTypes, judge, judgeGroup, project, submission } from '~/db/schema'
 import { Category, CategoryType, Judge, JudgeGroup, NewAssignment } from '~/db/types'
 import { getDb, RotatingQueue } from '~/utils'
+import { strictEqual } from 'assert'
 
 const devPostCsvColsMapping = {
 	'Project Title': 'title',
@@ -407,6 +408,16 @@ export const listAssignments = query(async () => {
 export const assignSubmissionsToJudgeGroups = action(async () => {
   'use server'
   const db = getDb()
+
+  // Preconditions: Each project should be seen by at least 6 judges, meaning we must have >= 6 General judges, since some projects only submit to General
+  const MINIMUM_JUDGES_PER_PROJECT = 6
+  const countGeneralJudges = await db.$count(judge, eq(judge.categoryId, 1))
+  if (countGeneralJudges < MINIMUM_JUDGES_PER_PROJECT) {
+    throw Error(`There must be at least ${MINIMUM_JUDGES_PER_PROJECT} General judges`);
+  }
+
+  await db.delete(assignment)
+
   const allSubmissions = await db.query.submission.findMany({ with: { category: true }})
   const allGroups = await db.select({
     id: judgeGroup.id,
@@ -414,7 +425,7 @@ export const assignSubmissionsToJudgeGroups = action(async () => {
     judgeCount: sql<number>`count(${judge.id})`.mapWith(Number)
   })
   .from(judgeGroup)
-  .innerJoin(judge, eq(judge.id, judgeGroup.id))
+  .leftJoin(judge, eq(judgeGroup.id, judge.judgeGroupId))
   .groupBy(judgeGroup.id)
 
   const groupsByCategory = allGroups.reduce((acc, curr) => {
@@ -423,8 +434,8 @@ export const assignSubmissionsToJudgeGroups = action(async () => {
     } else {
       acc.get(curr.categoryId)!.enqueue(curr); // custom method we'll define
     }
-    return acc;
-  }, new Map<Category['id'], RotatingQueue<typeof allGroups[number]>>());
+    return acc
+  }, new Map<Category['id'], RotatingQueue<typeof allGroups[number]>>())
 
   const PHASE_1_JUDGE_GROUPS_PER_PROJECT: Record<Category['type'], number> = {
     'inhouse': 2,
@@ -436,24 +447,67 @@ export const assignSubmissionsToJudgeGroups = action(async () => {
   // Project assignment algorithm:
   // Phase 1: Assign submissions to judge groups of corresponding category, ensuring JUDGE_GROUPS_PER_PROJECT groups per project, while recording how many judges per project
   // Phase 2: Make additional General assignments if any projects have less than MINIMUM_JUDGE_PER_PROJECT judges
-  const judgeCountPerProject = new Map()
-  const assignmentsToInsert = allSubmissions.flatMap(s => {
-    const judgeGroupsPerSubmission = Math.min(groupsByCategory.get(s.categoryId)?.length || 0, PHASE_1_JUDGE_GROUPS_PER_PROJECT[s.category.type])
-    return Array.from({ length: judgeGroupsPerSubmission }).map(() => {
+  const assignmentsByProject = allSubmissions.reduce((acc, s) => {
+    const howManyJudgeGroups = groupsByCategory.get(s.categoryId)?.length || 0
+    const suggestedJudgeGroupNumber = PHASE_1_JUDGE_GROUPS_PER_PROJECT[s.category.type]
+    const judgeGroupsPerSubmission = Math.min(howManyJudgeGroups, suggestedJudgeGroupNumber)
+
+    const assignments = Array.from({ length: judgeGroupsPerSubmission }).map(() => {
       const groupToAssign = groupsByCategory.get(s.categoryId)!.getNext()
-
-      // Update judge count of this project
-      const currentJudgeCount = judgeCountPerProject.get(s.projectId) || 0
-      judgeCountPerProject.set(s.projectId, groupToAssign.judgeCount + currentJudgeCount)
-
-      return {
-        submissionId: s.id,
-        judgeGroupId: groupToAssign.id,
-      }
+      return { submissionId: s.id, judgeGroup: groupToAssign }
     })
-)
-  const a = db.insert(assignment).values(assignmentsToInsert).toSQL()
-  console.log(a)
 
+    acc.set(s.projectId, (acc.get(s.projectId) || []).concat(assignments))
+
+    return acc
+  }, new Map<number, Array<{ submissionId: number, judgeGroup: typeof allGroups[number]}>>())
+
+  // Phase 2: Assign additional General groups
+  for (const [projectId, assignments] of assignmentsByProject) {
+    let howManyJudgesThisProject = assignments.reduce((acc, curr) => acc + curr.judgeGroup.judgeCount, 0)
+    const GENERAL_CATEGORY_ID = 1
+    const generalSubmissionOfThisProject = allSubmissions.find(s => s.projectId === projectId && s.categoryId === GENERAL_CATEGORY_ID)!
+    while (howManyJudgesThisProject < MINIMUM_JUDGES_PER_PROJECT) {
+      const nextGeneralGroup = groupsByCategory.get(GENERAL_CATEGORY_ID)!.getNext()
+      const currentGroups = assignments.map(a => a.judgeGroup.id)
+      if (!currentGroups.includes(nextGeneralGroup.id)) {
+        assignments.push({ submissionId: generalSubmissionOfThisProject.id, judgeGroup: nextGeneralGroup })
+        howManyJudgesThisProject += nextGeneralGroup.judgeCount
+      }
+    }
+  }
+
+
+  // Some final post-condition checks
+  const projectsWithInsufficientJudges = [...assignmentsByProject.entries()]
+    .map(([projectId, assignments]) => ([ projectId, assignments.reduce((acc, curr) => acc + curr.judgeGroup.judgeCount, 0) ]) as const)
+    .filter(([_, judgeCount]) => judgeCount < MINIMUM_JUDGES_PER_PROJECT )
+
+  strictEqual(projectsWithInsufficientJudges.length, 0)
+
+  const assignmentsToInsert = [...assignmentsByProject.values()]
+    .flatMap(
+      assignments => assignments.map(a => ({ submissionId: a.submissionId, judgeGroupId: a.judgeGroup.id} ))
+    )
+
+  const seen = new Set();
+  const duplicates = [];
+  for (const entry of assignmentsToInsert) {
+    const key = `${entry.submissionId}:${entry.judgeGroupId}`;
+    if (seen.has(key)) {
+      duplicates.push(entry);
+    } else {
+      seen.add(key);
+    }
+  }
+
+  strictEqual(duplicates.length, 0)
+
+  // Split into batch because Cloudflare D1 has limit on SQL statement size
+  const BATCH_SIZE = 50;
+  for (let i = 0; i < assignmentsToInsert.length; i += BATCH_SIZE) {
+    const batch = assignmentsToInsert.slice(i, i + BATCH_SIZE);
+    await db.insert(assignment).values(batch);
+  }
 }, 'assign-submissions-to-judge-groups')
 
