a7a09f5cbb804ef395d07c50af52d46cd173f8be
bhuynhdev <baoh.inbox@gmail.com>
2025-07-29 22:20:35 -0400

    Auth middleware: Validate session cookie and assign event.locals.user

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,13 @@
+import { redirect } from '@solidjs/router'
 import { createMiddleware } from '@solidjs/start/middleware'
+import { and, eq, lte } from 'drizzle-orm'
 import { drizzle as drizzleD1 } from 'drizzle-orm/d1'
+import { getCookie } from 'vinxi/http'
 import * as schema from './db/schema'
+import { session, user } from './db/schema'
+import { sha512_256 } from '@oslojs/crypto/sha2'
+import { constantTimeEqual } from '@oslojs/crypto/subtle'
+import { removeSessionCookie } from './features/auth/auth'
 
 export default createMiddleware({
 	onRequest: async (event) => {
@@ -15,5 +22,37 @@ export default createMiddleware({
 				event.locals.db = db
 			}
 		}
+
+    if (!event.locals.user) {
+      const sessionToken = getCookie("session_token")
+      // Validate session token
+      if (!sessionToken) {
+        return redirect("/admin", 302)
+      }
+      const tokenParts = sessionToken.split(".");
+      if (tokenParts.length !== 2) {
+        removeSessionCookie()
+        return redirect("/admin", 403);
+      }
+      const [sessionId, sessionSecret] = tokenParts;
+      const [existingSession] = await event.locals.db.select().from(session).where(and(eq(session.id, sessionId), lte(session.expiresAt, new Date()))).limit(1)
+      if (!existingSession) {
+        removeSessionCookie()
+        return redirect("/admin", 403)
+      }
+      const tokenSecretHash = sha512_256(new TextEncoder().encode(sessionSecret));
+      const isValidSecret = constantTimeEqual(tokenSecretHash, existingSession.secretHash)
+      if (!isValidSecret) {
+        removeSessionCookie()
+        return redirect("/admin", 403)
+      }
+
+      const [authenticatedUser] = await event.locals.db.select().from(user).where(eq(user.id, existingSession.userId))
+      if (!authenticatedUser) {
+        removeSessionCookie()
+        return redirect("/admin", 403)
+      }
+      event.locals.user = authenticatedUser
+    }
 	}
 })
