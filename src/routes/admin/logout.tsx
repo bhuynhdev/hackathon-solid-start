91e10503105fc51b2a53df6f8f75cc488e82756a
bhuynhdev <baoh.inbox@gmail.com>
2025-07-29 22:20:56 -0400

    Logout route: Clear cookie and delete session from DB

---
+++ src/routes/admin/logout.tsx
@@ -0,0 +1,19 @@
+import { redirect } from "@solidjs/router";
+import type { APIEvent } from "@solidjs/start/server";
+import { eq } from "drizzle-orm";
+import { getCookie } from "vinxi/http";
+import { session } from "~/db/schema";
+import { removeSessionCookie, SESSION_TOKEN_COOKIE_NAME } from "~/features/auth/auth";
+import { getDb } from "~/utils";
+
+export async function GET(event: APIEvent) {
+  const currentToken = getCookie(SESSION_TOKEN_COOKIE_NAME)
+  if (currentToken) {
+    removeSessionCookie()
+    const db = getDb()
+    await db.delete(session).where(eq(session.id, currentToken.split(".")[0]))
+  }
+
+  return redirect("/admin", 302)
+}
+
